76b2b4c2e22279565e324f1630eb27a1d6dcf51e
asimxdev <asimxdev@gmail.com>
2025-01-26 15:15:34 +0500

    shared preference code

---
+++ lib/splash_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Image(
+          image: NetworkImage(
+              'https://images.pexels.com/photos/323682/pexels-photo-323682.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2')),
+    );
+  }
+}
