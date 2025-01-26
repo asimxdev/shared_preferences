8ac7faeb6c890df766dc5dfbded2099c6eed608d
asimxdev <asimxdev@gmail.com>
2025-01-26 16:05:26 +0500

    Commit Messsage

--- lib/splash_screen.dart
+++ lib/splash_screen.dart
@@ -1,14 +1,54 @@
-import 'package:flutter/material.dart';
+import 'dart:async';
+import 'dart:ui';
 
-class SplashScreen extends StatelessWidget {
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:shared_preferences1/home_screen.dart';
+
+class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
 
   @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    Timer(
+      Duration(seconds: 5),
+      () {
+        Navigator.pushAndRemoveUntil(
+          context,
+          CupertinoPageRoute(
+              builder: (context) {
+                return HomeScreen();
+              },
+              fullscreenDialog: false),
+          (route) => false,
+        );
+      },
+    );
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Image(
-          image: NetworkImage(
-              'https://images.pexels.com/photos/323682/pexels-photo-323682.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2')),
+    // final screenHeight = MediaQuery.sizeOf(context).height;
+    // final screenWidth = MediaQuery.sizeOf(context).width;
+    return Container(
+      decoration: BoxDecoration(
+          image: DecorationImage(
+              fit: BoxFit.cover,
+              image: NetworkImage(
+                  'https://images.pexels.com/photos/842571/pexels-photo-842571.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'))),
+      child: BackdropFilter(
+        filter: ImageFilter.blur(sigmaX: 3, sigmaY: 3),
+        child: Scaffold(
+          backgroundColor: Colors.transparent,
+        ),
+      ),
     );
   }
 }
